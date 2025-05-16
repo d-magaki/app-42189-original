7b11f7b2b577f7052457405d554e81cede53ab32
d-magaki <dkmk.03112600@gmail.com>
2025-05-17 00:25:51 +0900

    デプロイ修正5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,39 @@
-# db/seeds.rb
-
 puts "✅ シードデータ登録開始..."
 
 # 共通ユーザー
 admin_user = User.find_or_create_by!(email: "admin@example.com") do |user|
   user.employee_id = "EMP9999"
   user.user_name = "管理者ユーザー"
-  user.department = "企画部"
-  user.role = "管理者"
+  user.department = :planning
+  user.role = :admin
   user.password = "password123"
   user.password_confirmation = "password123"
 end
 
 # 各部署の一般ユーザー
-企画担当 = User.find_or_create_by!(email: "planner@example.com") do |user|
+planner = User.find_or_create_by!(email: "planner@example.com") do |user|
   user.employee_id = "EMP1001"
   user.user_name = "企画担当"
-  user.department = "企画部"
-  user.role = "一般社員"
+  user.department = :planning
+  user.role = :employee
   user.password = "password123"
   user.password_confirmation = "password123"
 end
 
-設計担当 = User.find_or_create_by!(email: "designer@example.com") do |user|
+designer = User.find_or_create_by!(email: "designer@example.com") do |user|
   user.employee_id = "EMP1002"
   user.user_name = "設計担当"
-  user.department = "情報設計部"
-  user.role = "一般社員"
+  user.department = :design
+  user.role = :employee
   user.password = "password123"
   user.password_confirmation = "password123"
 end
 
-開発担当 = User.find_or_create_by!(email: "developer@example.com") do |user|
+developer = User.find_or_create_by!(email: "developer@example.com") do |user|
   user.employee_id = "EMP1003"
   user.user_name = "開発担当"
-  user.department = "開発部"
-  user.role = "一般社員"
+  user.department = :development
+  user.role = :employee
   user.password = "password123"
   user.password_confirmation = "password123"
 end
@@ -51,12 +49,12 @@ Project.create!(
   customer_name: "企画未着手株式会社",
   sales_office: "東京",
   sales_representative: "営業太郎",
-  request_type: "新規依頼",
-  request_content: "WEBアプリ制作",
+  request_type: :新規依頼,
+  request_content: :WEBアプリ制作,
   order_date: Date.today - 10,
   due_date: Date.today + 20,
-  status: "未着手",
-  planning_user: 企画担当,
+  status: :未着手,
+  planning_user: planner,
   user: admin_user
 )
 
@@ -64,12 +62,12 @@ Project.create!(
   customer_name: "企画進行中株式会社",
   sales_office: "東京",
   sales_representative: "営業次郎",
-  request_type: "修正依頼",
-  request_content: "WEBデザイン制作",
+  request_type: :修正依頼,
+  request_content: :WEBデザイン制作,
   order_date: Date.today - 15,
   due_date: Date.today + 10,
-  status: "進行中",
-  planning_user: 企画担当,
+  status: :進行中,
+  planning_user: planner,
   planning_start_date: Date.today - 5,
   user: admin_user
 )
@@ -78,15 +76,15 @@ Project.create!(
   customer_name: "設計未着手株式会社",
   sales_office: "大阪",
   sales_representative: "営業三郎",
-  request_type: "バグ修正",
-  request_content: "システム構築",
+  request_type: :バグ修正,
+  request_content: :システム構築,
   order_date: Date.today - 20,
   due_date: Date.today + 5,
-  status: "未着手",
-  planning_user: 企画担当,
+  status: :未着手,
+  planning_user: planner,
   planning_start_date: Date.today - 10,
   planning_end_date: Date.today - 5,
-  design_user: 設計担当,
+  design_user: designer,
   user: admin_user
 )
 
@@ -94,15 +92,15 @@ Project.create!(
   customer_name: "設計進行中株式会社",
   sales_office: "名古屋",
   sales_representative: "営業四郎",
-  request_type: "追加依頼",
-  request_content: "データ解析",
+  request_type: :追加依頼,
+  request_content: :データ解析,
   order_date: Date.today - 30,
   due_date: Date.today + 7,
-  status: "進行中",
-  planning_user: 企画担当,
+  status: :進行中,
+  planning_user: planner,
   planning_start_date: Date.today - 20,
   planning_end_date: Date.today - 15,
-  design_user: 設計担当,
+  design_user: designer,
   design_start_date: Date.today - 10,
   user: admin_user
 )
@@ -111,18 +109,18 @@ Project.create!(
   customer_name: "開発未着手株式会社",
   sales_office: "福岡",
   sales_representative: "営業五郎",
-  request_type: "新規依頼",
-  request_content: "スマホアプリ制作",
+  request_type: :新規依頼,
+  request_content: :スマホアプリ制作,
   order_date: Date.today - 25,
   due_date: Date.today + 14,
-  status: "未着手",
-  planning_user: 企画担当,
+  status: :未着手,
+  planning_user: planner,
   planning_start_date: Date.today - 20,
   planning_end_date: Date.today - 15,
-  design_user: 設計担当,
+  design_user: designer,
   design_start_date: Date.today - 14,
   design_end_date: Date.today - 10,
-  development_user: 開発担当,
+  development_user: developer,
   user: admin_user
 )
 
@@ -130,18 +128,18 @@ Project.create!(
   customer_name: "開発進行中株式会社",
   sales_office: "札幌",
   sales_representative: "営業六郎",
-  request_type: "その他＿依頼",
-  request_content: "その他＿内容",
+  request_type: :その他＿依頼,
+  request_content: :その他＿内容,
   order_date: Date.today - 40,
   due_date: Date.today + 3,
-  status: "進行中",
-  planning_user: 企画担当,
+  status: :進行中,
+  planning_user: planner,
   planning_start_date: Date.today - 30,
   planning_end_date: Date.today - 25,
-  design_user: 設計担当,
+  design_user: designer,
   design_start_date: Date.today - 24,
   design_end_date: Date.today - 20,
-  development_user: 開発担当,
+  development_user: developer,
   development_start_date: Date.today - 5,
   user: admin_user
 )
